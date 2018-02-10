11db214799839312cb51b0cd38e02237e216684b
Chris Whitty <crswty@users.noreply.github.com>
2018-02-10 00:14:45 +0000

    Provide clicked view in click observable

--- kind-integration-test/src/test/kotlin/com/crswty/kind/integration/ViewDelegateTest.kt
+++ kind-integration-test/src/test/kotlin/com/crswty/kind/integration/ViewDelegateTest.kt
@@ -5,8 +5,8 @@ import com.crswty.kind.bind
 import com.crswty.kind.integration.activity.R
 import com.crswty.kind.integration.activity.StandardWidgetsActivity
 import com.crswty.kind.integration.util.TestViewProvider
-import org.hamcrest.Matchers
-import org.junit.Assert
+import org.hamcrest.Matchers.equalTo
+import org.hamcrest.Matchers.hasSize
 import org.junit.Assert.assertThat
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -25,14 +25,16 @@ class ViewDelegateTest {
         }
         val textView = activity.findViewById<CheckBox>(R.id.checkbox_view)
 
-        val events = mutableListOf<Unit>()
+        val events = mutableListOf<CheckBox>()
         provider.clickObservable
                 .subscribe({ events.add(it) })
 
         textView.performClick()
         textView.performClick()
 
-        assertThat(events, Matchers.hasSize(2))
+        assertThat(events, hasSize(2))
+        assertThat(events[0], equalTo(textView))
+        assertThat(events[1], equalTo(textView))
     }
 }
 
--- kind/src/main/kotlin/com/crswty/kind/ViewDelegate.kt
+++ kind/src/main/kotlin/com/crswty/kind/ViewDelegate.kt
@@ -8,9 +8,10 @@ import kotlin.reflect.KProperty
 class ViewDelegate<V : View>(id: Int): ReadPropertyAndroidDelegate<V, V>(id) {
     override fun getFromView(view: V, prop: KProperty<*>) = view
 
-    val clickObservable = ObservableDelegate<View, Unit>(id) { view, emitter ->
-        view.setOnClickListener {
-            emitter.onNext(Unit)
+    val clickObservable = ObservableDelegate<V, V>(id) { view, emitter ->
+        view.setOnClickListener { view ->
+            @Suppress("UNCHECKED_CAST")
+            emitter.onNext(view as V)
         }
     }
 
